ad979db9ccc2456cd12197408687c77d4734d0f5
Manyaka <104553245+237Codes@users.noreply.github.com>
2025-02-11 01:13:07 -0500

    Run Java file in IntelliJ

---
+++ my_first_program.java
@@ -0,0 +1,8 @@
+public class my_first_program {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        double c = a + b;
+        System.out.println( a + " + " + b + " = " + c);
+    }
+}
