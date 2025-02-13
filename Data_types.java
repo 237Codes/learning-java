04f7f18268a7ebc9e72b08aa98772c6061063e1c
Manyaka <104553245+237Codes@users.noreply.github.com>
2025-02-13 01:38:17 -0500

    add data types practice

---
+++ Data_types.java
@@ -0,0 +1,18 @@
+class Data_types {
+    public void greet() {
+        System.out.println("Hello World");
+
+        // Testing out data types
+        boolean booleanVar; //This is a boolean
+
+        //Non-primitive data types are reference typed
+
+        // Declaring a string
+        // <String_Type> <string_variable> = "<sequence_of_string>"
+
+        String myString = "This is my string";
+        System.out.println(myString);
+
+
+    }
+}
--- my_first_program.java
+++ my_first_program.java
@@ -1,8 +1,13 @@
+
+import java.util.*;
 public class my_first_program {
     public static void main(String[] args) {
         int a = 10;
         int b = 5;
         double c = a + b;
         System.out.println( a + " + " + b + " = " + c);
+
+       Data_types d = new Data_types();
+        d.greet();
     }
 }
